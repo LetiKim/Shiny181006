302dd5654c33dee0da2a075efc62fedea318f492
sanae618 <sanae618@hanmail.net>
2018-10-13 18:16:18 +0900

    first commit

---
+++ shiny_pop.R
@@ -0,0 +1,47 @@
+if("plotly" %in% installed.packages("plotly") == FALSE)install.packages("plotly")
+library(plotly)
+library(shiny)
+data <- data.frame(
+  population <- sample(1:20,10,replace = T),
+  HouseHolds <- sample(1:20,10,replace = T),
+  year <- sample(c(2000,2010),10,replace = T)
+)
+
+ui <- fluidPage(
+  titlePanel(
+    title = h4("인구 조사",align = "center")
+),
+sidebarPanel(
+  radioButtons("YEAR","10년주기선택",
+               choices = c("2000","2010"),
+               selecte = "2000"
+               )
+),
+mainPanel(
+  plotOutput("bar",height = 500)
+)
+)
+server <- function(input, output, session) {
+reactive_data <- reactive({
+  selected_year <- as.numeric(input$YEAR)
+  return(data[data$year == selected_year])
+})
+output$bar <- renderPlot({
+  color <- c("blue", "red")
+  our_data <- reactive_data()
+  barplot(colSums(our_data[,c("Population","Households")]),
+          ylab="Total",
+          xlab="Census Year",
+          names.arg = c("Population", "Households"),
+          col = color)
+})
+}
+shinyApp(ui, server)
+
+
+
+
+
+
+
+
